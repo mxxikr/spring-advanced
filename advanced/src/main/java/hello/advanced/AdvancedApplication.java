fc3e0ebfc80dc8af8e73fc34fcf41e4ba8912497
mxxikr <mxxikr@gmail.com>
2026-02-28 20:08:01 +0900

    쓰레드 로컬 - ThreadLocal: 쓰레드 로컬 동기화 - 적용

--- advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,20 +1,12 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
-import hello.advanced.trace.logtrace.LogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
-	}
-
-	@Bean
-	public LogTrace logTrace() {
-		return new FieldLogTrace();
 	}
 }

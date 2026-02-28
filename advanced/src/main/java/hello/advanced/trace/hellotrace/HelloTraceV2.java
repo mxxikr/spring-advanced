29cce4862a926434995815771fa6483bbc0b0640
mxxikr <mxxikr@gmail.com>
2026-02-28 13:44:07 +0900

    예제 만들기: 로그 추적기 V2 - 적용

--- advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -15,17 +15,17 @@ public class HelloTraceV2 {
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("[" + traceId.getId() +  "]" + addSpace(START_PREFIX, traceId.getLevel()) + message);
+        log.info("[" + traceId.getId() + "] " + addSpace(START_PREFIX, traceId.getLevel()) + message);
 
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
     // V2에서 추가
-    public TraceStatus beginSync(TraceId beforeTraceId,String message) {
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
         TraceId nextId = beforeTraceId.createNextId();
         Long startTimeMs = System.currentTimeMillis();
 
-        log.info("[" + nextId.getId() + "]" + addSpace(COMPLETE_PREFIX, nextId.getLevel()) + message);
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
 
         return new TraceStatus(nextId, startTimeMs, message);
     }

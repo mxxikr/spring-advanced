9a8261597728a5c4380b71ce6fe4172ec8699796
mxxikr <mxxikr@gmail.com>
2026-03-04 05:51:58 +0900

    프록시 패턴과 데코레이터 패턴: 구체 클래스 기반 프록시 - 예제2

--- proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -2,7 +2,7 @@ package hello.proxy.pureproxy.concreteproxy.code;
 
 public class ConcreteClient {
 
-    private final ConcreteLogic concreteLogic;
+    private final ConcreteLogic concreteLogic; //ConcreteLogic, TimeProxy 모두 주입 가능
 
     public ConcreteClient(ConcreteLogic concreteLogic) {
         this.concreteLogic = concreteLogic;

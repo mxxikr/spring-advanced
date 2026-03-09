4ea08d80f007f2e64ccebe610c8b49ce509c3d5e
mxxikr <mxxikr@gmail.com>
2026-03-09 20:03:32 +0900

    스프링 AOP - 실무 주의사항: 프록시와 내부 호출 - 문제

---
+++ aop/src/test/java/hello/aop/pointcut/CallServiceV1Test.java
@@ -0,0 +1,26 @@
+package hello.aop.pointcut;
+
+import hello.aop.internalcall.CallServiceV1;
+import hello.aop.internalcall.aop.CallLogAspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+public class CallServiceV1Test {
+
+    @Autowired
+    CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV1.interanl();
+    }
+}

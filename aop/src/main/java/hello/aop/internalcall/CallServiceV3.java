93809a8a3a2ff603fdbf7dc443dc2feb990c6427
mxxikr <mxxikr@gmail.com>
2026-03-09 20:14:27 +0900

    스프링 AOP - 실무 주의사항: 프록시와 내부 호출 - 대안3 구조 변경

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV3.java
@@ -0,0 +1,22 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext를 사용해서 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV3 {
+
+    private final InternalService internalService;
+
+    public void external() {
+        log.info("call external");
+        internalService.interanl();
+    }
+}
